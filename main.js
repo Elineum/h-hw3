80756309c1f62e2b0aa316edd96556bdfd08b140
Elineum <you@example.com>
2023-05-25 22:45:53 +0300

    fix seconds and variable in if

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 const hours = +prompt("Enter number of hours", "");
-let seconds;
 
 if (hours) {
-  seconds = hours * 60;
+  let seconds = hours * 60 * 60;
   alert(`Your hours in seconds looks like this: ${seconds}sec`);
 } else {
   alert(`You enter uncorrect value :( \nTry again!`);
