97049512577140a22179e78eaba95dd5a8c897e1
Elineum <you@example.com>
2023-05-25 22:53:17 +0300

    ta za sho :(

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const hours = +prompt("Enter number of hours", "");
 
 if (hours) {
-  let seconds = hours * 60 * 60;
+  const seconds = hours * 60 * 60;
   alert(`Your hours in seconds looks like this: ${seconds}sec`);
 } else {
   alert(`You enter uncorrect value :( \nTry again!`);
